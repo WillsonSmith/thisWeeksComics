9b4ec11853472caf25c3053adb837374a55f2b94
Willson Smith <willsonsmth@gmail.com>
2014-05-20 21:21:52 -0400

    cleanup of fmtDate

---
+++ func/fmtDate.js
@@ -0,0 +1,21 @@
+var moment = require('moment');
+var format = function(item) {
+	//console.log(item);
+	//console.log(item);
+
+	item.forEach( function(i){
+		var date = {type: 'sale', 'date': ''};
+
+		if (i.type === 'onsaleDate') {
+
+			date.date = moment(i.date).format('D MMM YYYY');
+
+			i.dates = date;
+
+		}
+
+	});
+
+};
+
+module.exports.fmt = format;
